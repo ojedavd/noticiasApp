b1ea2cb0a5113dc1a8ecb55156dd8ed44e057c0b
Ojeda Victor <ojedavd@gmail.com>
2019-08-23 18:28:16 -0300

    Compartir noticia

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Article } from 'src/app/interfaces/interfaces';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 import { ActionSheetController } from '@ionic/angular';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @Component({
   selector: 'app-noticia',
@@ -14,7 +15,8 @@ export class NoticiaComponent implements OnInit {
   @Input() indice: number;
 
   constructor(private iab: InAppBrowser,
-              private actionSheetCtrl: ActionSheetController) { }
+              private actionSheetCtrl: ActionSheetController,
+              private socialSharing: SocialSharing) { }
 
   ngOnInit() {}
 
@@ -31,6 +33,12 @@ export class NoticiaComponent implements OnInit {
         cssClass: 'action-dark',
         handler: () => {
           console.log('Share clicked');
+          this.socialSharing.share(
+            this.noticia.title,
+            this.noticia.source.name,
+            '',
+            this.noticia.url
+          );
         }
       }, {
         text: 'Favorito',
