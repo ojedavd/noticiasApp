3498375d4b3ac7936c94ba29227d25b5f4d42321
Victor <ojedavd@gmail.com>
2019-08-22 22:48:59 -0300

    Componentes de noticias y noticia

--- src/app/pages/tab1/tab1.module.ts
+++ src/app/pages/tab1/tab1.module.ts
@@ -1,13 +1,15 @@
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
   imports: [
     IonicModule,
+    ComponentsModule,
     CommonModule,
     FormsModule,
     RouterModule.forChild([{ path: '', component: Tab1Page }])
